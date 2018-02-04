2e62fbace36030b9590a47186c0c9c1bb61cad6b
Blakeley Bond <burrick2003@yahoo.com>
2018-02-04 23:30:55 +0000

    automatically backed up by learn

--- db/migrate/20180204232248_create_movies.rb
+++ db/migrate/20180204232248_create_movies.rb
@@ -1,4 +1,11 @@
 class CreateMovies < ActiveRecord::Migration
   def change
+    create_table :movies do |t| #cutnpaste from create_costumes
+      t.string :title
+      t.integer :release_date
+      t.string :director
+      t.string :lead
+      t.boolean :in_theaters
+      t.timestamps null: false #knows when it was created / updated
   end
 end
